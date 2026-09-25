1619affd3a9aef7474dd884580479d0508abc6f8
Charley Wu <akunzai@gmail.com>
2026-09-25 11:12:15 +0800

    chore(release): bump version to v0.16.0

--- internal/updater/updater.go
+++ internal/updater/updater.go
@@ -20,7 +20,7 @@ import (
 	"github.com/akunzai/skills-manager/internal/models"
 )
 
-var Version = "0.15.0"
+var Version = "0.16.0"
 var GitHubRepo = "akunzai/skills-manager"
 
 type ReleaseAsset struct {
